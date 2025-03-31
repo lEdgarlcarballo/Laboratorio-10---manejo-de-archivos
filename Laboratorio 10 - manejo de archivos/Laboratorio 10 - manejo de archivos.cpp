f549e9cb8b33e55b234c4bc7e1d53312f35d9b57
Edgar Carballo Ortega <carballo240050@gmail.com>
2025-03-31 11:37:22 -0600

    declaracion del constructor y metodos

--- Laboratorio 10 - manejo de archivos/Laboratorio 10 - manejo de archivos.cpp
+++ Laboratorio 10 - manejo de archivos/Laboratorio 10 - manejo de archivos.cpp
@@ -2,18 +2,119 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n"; as
+using namespace std;
+
+class MiembroClub {
+private:
+	string nombre;
+	int edad;
+	string correo;
+
+public:
+	MiembroClub(string _n, int _e, string _c) {
+		nombre = _n;
+		edad = _e;
+		correo = _c;
+	}
+	//-----------------------------------------------------------------------------------------------------------------
+void guardarEnArchivo() {
+		ofstream archivo("club.txt ", ios::app);
+		if (archivo.is_open()) {
+			archivo << nombre << " " << edad << " " << correo << endl;
+			archivo.close();
+			cout << "Datos guardados exitosamente.\n";
+		}
+		else {
+			cout << "Error al abrir el archivo.\n";
+		}
+	}
+//-----------------------------------------------------------------------------------------------------------------
+static void leerDesdeArchivo() {
+	ifstream archivo("club.txt");
+	string n, c;
+	int e;
+	if (archivo.is_open()) {
+		cout << "\nLista de Miembros:\n";
+		while (archivo >> n >> e >> c) {
+			cout << "Nombre: " << n << ", Edad: " << e << ", Correo: " << c << endl;
+		}
+		archivo.close();
+	}
+	else {
+		cout << "Error al abrir el archivo o el archivo no existe.\n";
+	}
 }
+//-----------------------------------------------------------------------------------------------------------------
+static void actualizarMiembros(string nombreBuscar,int nuevaEdad, string correoNuevo) {
+	ifstream archivo("club.txt");
+	ofstream archivoTemp("temp.txt");
+	string n, c;
+	int e;
+	bool encontrado = false;
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+	if (archivo.is_open() && archivoTemp.is_open()) {
+		while (archivo >> n >> e >> c) {
+			if (n == nombreBuscar) {
+				archivoTemp << n << " " << nuevaEdad << " " << correoNuevo << endl;
+				encontrado = true;
+			}
+			else {
+				archivoTemp << n << " " << e << " " << c << endl;
+			}
+		}
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+		archivo.close();
+		archivoTemp.close();
+		remove("club.txt");
+		rename("temp.txt", "club.txt");
+		if (encontrado) {
+			cout << "Miembro actualizado exitosamente.\n";
+		}
+		else {
+			cout << "Miembro no encontrado.\n";
+		}
+
+	}
+	else {
+		cout << "Error al abrir el archivo.\n";
+
+	}
+}
+//-----------------------------------------------------------------------------------------------------------------
+static void eliminarMiembro(string nombreBuscar) {
+	ifstream archivo("club.txt");
+	ofstream archivoTemp("temp.txt");
+	string n, c;
+	int e;
+	bool eliminado = false;
+	
+	if (archivo.is_open() && archivoTemp.is_open()) {
+		while (archivo >> n >> e >> c) {
+			if (n == nombreBuscar) {
+				eliminado = true;
+			}
+			else {
+				archivoTemp << n << " " << e << " " << c << endl;
+			}
+		}
+
+		archivo.close();
+		archivoTemp.close();
+		remove("club.txt");
+		rename("temp.txt", "club.txt");
+		if (eliminado) {
+			cout << "miembro eliminado exitosamente." << endl;
+		}
+		else {
+			cout << "Miembro no enconstrado " << endl;
+		}
+	}
+	else {
+		cout << "Error al abrir el archivo." << endl;
+	}
+}
+};
+
